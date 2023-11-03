bd13f6be18c709ef6e525ef10d52ca9fbd89aae3
JakubKb <jakubsakw2009@gmail.com>
2023-11-03 21:50:53 +0100

    generating a password with just normal characters

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
+import random
 
-password_lenght = float(input("How many characters would you like? "))
+password_lenght = int(input("How many characters would you like? "))
+includes_chars = input("do you want normal characters?(yes/no)")
+includes_special_chars = input("do you want special characters?(yes/no)")
+includes_numbers = input("do you want numbers?(yes/no)")
 basic_chars = 'qwertyuiopasdfghjklzxcvbnm'
+numbers = '1234567890'
+special_chars = '~!@#$%^&*()_+{}|:"<>?-=[]\;.,'
+password = ""
+
+for x in range(password_lenght):
+    if includes_chars == "yes" and includes_numbers == "no" and includes_special_chars == "no":
+        password += random.choice(basic_chars)
+
+print(password)
